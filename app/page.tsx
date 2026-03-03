7c197ca74af29ea5ad62a79471226fcac5cbd999
claudio-inacio <inacio.unipar@gmail.com>
2026-03-03 17:11:30 -0300

    feat: create general header

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
-'use client'
+'use client';
 import useDashboardData from "./hooks/useDashboardData";
 import { useEffect } from "react";
+import HeaderComponent from "./components/header/HeaderComponent";
+import logoImage from "./assets/img/new-icon.png";
 
 export default function Home() {
   
   const { resultSet, handleGetDashboardData, requestLoading } = useDashboardData();
 
   useEffect(() => {
-    handleGetDashboardData();
-
+    handleGetDashboardData()
 
   }, [])
   
 
-  console.log({resultSet})
-
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col font-sans">
+      <HeaderComponent headerTittle="DASHBOARD DE MÉTRICAS" imageIcon={logoImage}/>
       <span className="">MEU TEXTO</span>
     </div>
   );
